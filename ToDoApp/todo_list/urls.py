b627018628fbfdb6ce851e46186c88719a8a9400
Henry Lin <henry.lin@appdirect.com>
2019-02-27 10:48:44 -0800

    updates

--- ToDoApp/todo_list/urls.py
+++ ToDoApp/todo_list/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.home, name="home")
-    
+    path('', views.home, name="home"),
+	path('delete/<list_id>', views.delete, name="delete"),
 ]
--- ToDoApp/todo_list/views.py
+++ ToDoApp/todo_list/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect 
 from .models import List
 from .forms import ListForm 
-from django.contrib import messages 
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -11,9 +11,16 @@ def home(request):
 		if form.is_valid():
 			form.save()
 			all_items = List.objects.all
-			messages.success(request, ('Item has been added to list!'))
+			messages.success(request, 'Item has been added to list!')
 			return render(request, 'home.html', {'all_items': all_items})
 
 	else:
 		all_items = List.objects.all
 		return render(request, 'home.html', {'all_items': all_items})
+
+def delete(request, list_id):
+	item = List.objects.get(pk=list_id)
+	item.delete()
+	messages.success(request, ('Item has been deleted!'))
+	return redirect('home')
+

2e84b4809290c9500abe145818cb191e9bcc46c6
Henry Lin <henry.lin@appdirect.com>
2019-02-27 12:49:12 -0800

    updates

--- ToDoApp/todo_list/views.py
+++ ToDoApp/todo_list/views.py
@@ -35,3 +35,18 @@ def uncross(request, list_id):
 	item.completed = False
 	item.save()
 	return redirect('home')
+
+def edit(request, list_id): 
+	if request.method == 'POST': 
+		item = List.objects.get(pk=list_id)
+
+		form = ListForm(request.POST or None, instance=item)
+
+		if form.is_valid():
+			form.save()
+			messages.success(request, 'Item has been edited!')
+			return redirect('home')
+	else:
+		item = List.objects.get(pk=list_id)
+		return render(request, 'edit.html', {'item': item})
+

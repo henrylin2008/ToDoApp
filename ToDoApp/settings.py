61895c91d55f7e44078a904e1b9ac7196dbb0ce1
hlin <henrylin2008@yahoo.com>
2020-07-27 11:21:23 -0700

    removed secure token

--- ToDoApp/settings.py
+++ ToDoApp/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tit$faw(0#8+4es^$rdo$ucdnpx2edkte+(59u8w53^5bg!q#i'
+SECRET_KEY = '<secret_key>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
